8884a031820a97c34744f140845102aa2f269d76
mob99-9 <jamesorozco20050@gmail.com>
2025-03-30 16:50:10 +0800

    rstrip function

---
+++ inside_rstrip_function.py
@@ -0,0 +1,27 @@
+
+#Prog01. rstrip() remove the space characters at the end of the string. 
+#Create a program that do the same functionality without using rstrip() function.
+
+#ask user for a string
+
+user_string = input("Please input a string: ")
+
+#create an empty string 
+
+new_string = ""
+
+#add the spaces from the right until the first non-space character
+
+for char in user_string:
+    if char == " ":
+        new_string += char
+    else:
+        break
+
+#add the non-space characters 
+
+new_string += user_string.strip()
+
+#print the new string
+
+print(new_string)
