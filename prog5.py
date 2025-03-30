8012c4e757ab83c61347db37e3e31f4388ed088f
mob99-9 <jamesorozco20050@gmail.com>
2025-03-30 19:05:38 +0800

    startswith function

---
+++ prog5.py
@@ -0,0 +1,15 @@
+#Prog05. startswith() check if the string beginning part matches the function parameter.
+#Create a program that do the same functionality without using startswith() function.
+
+#ask user for a string and the letter to check if that is in the beginning
+
+user_string = input("Please enter a string: ")
+
+start_letter = input("startswith() please enter a character: ")
+
+#remove the start letter to the user string using lstrip to check if it starts with it
+
+if user_string[::-1].endswith(start_letter[::-1]) == True:
+    print(True)
+else:
+    print(False)
